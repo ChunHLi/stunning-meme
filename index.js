8812b2f4b880420cf5d2a48cdf3a924a846b7088
Shawn Li <shawnli050298@gmail.com>
2016-04-21 12:55:50 -0400

    Added randomization of velocity, radius, color

--- index.js
+++ index.js
@@ -11,21 +11,26 @@ var createBall = function(){
   var yPos = Math.floor(Math.random() * c.height);
   var xDir = 1;
   var yDir = 1;
+  var xVel = Math.floor(Math.random() * 10);
+  var yVel = Math.floor(Math.random() * 10);
+  var rad = Math.floor(Math.random() * 50);
   var Ball = function(){
     ctx.beginPath();
-	  ctx.arc(xPos, yPos, 20, 0, 2 * Math.PI);
-	  ctx.fill();
-	  xPos = xPos + (2 * xDir);
-	  yPos = yPos + (2 * yDir);
+    ctx.fillStyle = 'hsl(' + 360 * Math.random() + ', 50%, 50%)';
+    ctx.arc(xPos, yPos, rad, 0, 2 * Math.PI);
+    ctx.fill();
+    xPos = xPos + (xVel * xDir);
+    yPos = yPos + (yVel * yDir);
   }
   var Bounce = function(){
-    if (Math.abs(xPos-c.width/2) >= c.width/2 - 10){
+    if (Math.abs(xPos-c.width/2) >= c.width/2 - rad){
 	    xDir = xDir * -1;
 	  }
-	  if (Math.abs(yPos-c.height/2) >= c.height/2 - 10){
+	  if (Math.abs(yPos-c.height/2) >= c.height/2 - rad){
 	    yDir = yDir * -1;
     }
-    //still needs part of function to bounce off other balls;
+    //still needs part of function to bounce off other balls; not
+    //edit: not really necessary
   }
   return {
     ball : Ball,
